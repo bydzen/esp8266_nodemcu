caa074991d2290a0e13dc3e873f00ec5a2abddc0
bydzen <svnteentthones@gmail.com>
2022-06-12 19:10:27 +0700

    dht 11 firebase blynk code

---
+++ dht11-led-firebase-blynk.ino
@@ -0,0 +1,206 @@
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
+#define BLYNK_TEMPLATE_ID "XXXXX"
+#define BLYNK_DEVICE_NAME "XXXXX"
+#define BLYNK_AUTH_TOKEN "XXXXX"
+
+// FirebaseESP8266.h must be included before ESP8266WiFi.h
+#include "FirebaseESP8266.h" // Install Firebase ESP8266 library
+
+#define FIREBASE_HOST "XXXXX" // Without http:// or https:// schemes
+#define FIREBASE_AUTH "XXXXX"
+#define WIFI_SSID "XXXXX"
+#define WIFI_PASSWORD "XXXXX"
+
+// Define FirebaseESP8266 data object
+FirebaseData firebaseData;
+FirebaseData ledData;
+
+FirebaseJson json;
+
+#define DHTPIN D6 // Connect Data pin of DHT to D6
+int led = D5;     // Connect LED to D5
+
+#include <ESP8266WiFi.h>
+#include <DHT.h> // Install DHT11 Library and Adafruit Unified Sensor Library
+
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "XXXXX";
+char pass[] = "XXXXX";
+
+BlynkTimer timer;
+
+// This function is called every time the Virtual Pin 0 state changes
+BLYNK_WRITE(V0)
+{
+    // Set incoming value from pin V0 to a variable
+    int value = param.asInt();
+
+    if (Firebase.setInt(firebaseData, "/FirebaseIOT/led", value))
+    {
+        if (value == 0)
+        {
+            digitalWrite(led, LOW);
+        }
+        else
+        {
+            digitalWrite(led, HIGH);
+        }
+        Serial.println("PASSED");
+        Serial.println("PATH: " + firebaseData.dataPath());
+        Serial.println("TYPE: " + firebaseData.dataType());
+        Serial.println("ETag: " + firebaseData.ETag());
+        Serial.println("------------------------------------");
+        Serial.println();
+    }
+    else
+    {
+        Serial.println("FAILED");
+        Serial.println("REASON: " + firebaseData.errorReason());
+        Serial.println("------------------------------------");
+        Serial.println();
+    }
+
+    // Update state
+    Blynk.virtualWrite(V1, value);
+}
+
+// This function is called every time the device is connected to the Blynk.Cloud
+BLYNK_CONNECTED()
+{
+    // Change Web Link Button message to "Congratulations!"
+    Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
+    Blynk.setProperty(V3, "onImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
+    Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
+}
+
+// This function sends Arduino's uptime every second to Virtual Pin 2.
+void myTimerEvent()
+{
+    // You can send any value at any time.
+    // Please don't send more that 10 values per second.
+    Blynk.virtualWrite(V2, millis() / 1000);
+}
+
+void setup()
+{
+
+    Serial.begin(9600);
+
+    Blynk.begin(auth, ssid, pass);
+    // You can also specify server:
+    // Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+    // Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+    // Setup a function to be called every second
+    timer.setInterval(1000L, myTimerEvent);
+
+    dht.begin();
+    pinMode(led, OUTPUT);
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to Wi-Fi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println();
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();
+
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+    Firebase.reconnectWiFi(true);
+}
+
+void sensorUpdate()
+{
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    float f = dht.readTemperature(false);
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t) || isnan(f))
+    {
+        Serial.println(F("Failed to read from DHT sensor!"));
+        return;
+    }
+
+    Serial.print(F("Humidity: "));
+    Serial.println(h);
+    Serial.print(F("%  Temperature: "));
+    Serial.println(t);
+    Serial.print(F("C  ,"));
+    Serial.println(f);
+    Serial.print(F("F  "));
+
+    if (Firebase.setFloat(firebaseData, "/FirebaseIOT/temperature", t))
+    {
+        Serial.println("PASSED");
+        Serial.println("PATH: " + firebaseData.dataPath());
+        Serial.println("TYPE: " + firebaseData.dataType());
+        Serial.println("ETag: " + firebaseData.ETag());
+        Serial.println("------------------------------------");
+        Serial.println();
+    }
+    else
+    {
+        Serial.println("FAILED");
+        Serial.println("REASON: " + firebaseData.errorReason());
+        Serial.println("------------------------------------");
+        Serial.println();
+    }
+
+    if (Firebase.setFloat(firebaseData, "/FirebaseIOT/humidity", h))
+    {
+        Serial.println("PASSED");
+        Serial.println("PATH: " + firebaseData.dataPath());
+        Serial.println("TYPE: " + firebaseData.dataType());
+        Serial.println("ETag: " + firebaseData.ETag());
+        Serial.println("------------------------------------");
+        Serial.println();
+    }
+    else
+    {
+        Serial.println("FAILED");
+        Serial.println("REASON: " + firebaseData.errorReason());
+        Serial.println("------------------------------------");
+        Serial.println();
+    }
+}
+void loop()
+{
+    sensorUpdate();
+
+    Blynk.run();
+    timer.run();
+
+    //  if (Firebase.getString(ledData, "/FirebaseIOT/led")){
+    //    Serial.println(ledData.stringData());
+    //    if (ledData.stringData() == "1") {
+    //    digitalWrite(led, HIGH);
+    //    }
+    //  else if (ledData.stringData() == "0"){
+    //    digitalWrite(led, LOW);
+    //    }
+    //  }
+
+    delay(250);
+}
