4ccd3ad491766ed255ddfde30e204904ebe0fda2
Sultan Kautsar <42274355+bydzen@users.noreply.github.com>
2022-06-17 00:05:45 +0700

    Deliver the best efficient code

--- dht11-led-automation/dht11-led-firebase-blynk.ino
+++ dht11-led-automation/dht11-led-firebase-blynk.ino
@@ -1,228 +1,140 @@
-// Blynk config authentication
 #define BLYNK_TEMPLATE_ID "XXXXX"
 #define BLYNK_DEVICE_NAME "XXXXX"
 #define BLYNK_AUTH_TOKEN "XXXXX"
 
-// FirebaseESP8266.h must be included before ESP8266WiFi.h
-// Install Firebase ESP8266 library
-#include "FirebaseESP8266.h"
-
-// Firebase host and auth without http:// or https:// schemes
 #define FIREBASE_HOST "XXXXX"
 #define FIREBASE_AUTH "XXXXX"
 
-// Wi-Fi Config
 #define WIFI_SSID "XXXXX"
 #define WIFI_PASSWORD "XXXXX"
 
-// Define FirebaseESP8266 data object
-FirebaseData firebaseData;
-
-// Parse the json
-FirebaseJson json;
-
-// Connect Data pin of DHT to D6
 #define DHTPIN D6
-// Connect LED to D5
-int led = D5;
+#define redLed D5
 
-// Install the ESP8266 Wi-Fi library
+#define DHTTYPE DHT11
+
+#include "FirebaseESP8266.h"
+
 #include <ESP8266WiFi.h>
-// Install DHT11 Library and Adafruit Unified Sensor Library
 #include <DHT.h>
 
-// Define the DHT11
-#define DHTTYPE DHT11
-DHT dht(DHTPIN, DHTTYPE);
-
-// Comment this out to disable prints and save space
-#define BLYNK_PRINT Serial
-
-// Include the Blynk simple ESP8266
 #include <BlynkSimpleEsp8266.h>
 
-// Auth token Blynk
-char auth[] = BLYNK_AUTH_TOKEN;
+FirebaseData firebaseData;
+FirebaseJson json;
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
+DHT dht(DHTPIN, DHTTYPE);
+
+int h;
+float t;
+float hTemp;
+float tTemp;
+bool dhtStatus = false;
+bool ledStatus = false;
+int secSS, minMM, hrsHH = 0;
+
+unsigned long theTime;
+long beforeTime;
+
+char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = WIFI_SSID;
 char pass[] = WIFI_PASSWORD;
 
-BlynkTimer timer;
-
-int count = 0;
-
-// This function is called every time the device is connected to the Blynk.Cloud
-BLYNK_CONNECTED()
-{
-  Serial.printf("\nBlynk Connected Successfully!\n");
-}
-
-// This function sends Arduino's uptime every second to Virtual Pin 2.
-void myTimerEvent()
-{
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V2, millis() / 1000);
-
-  //Uptime seconds
-  int uptimeInSeconds = millis() / 1000;
-
-  // Uptime conversion from second to minutes
-  int uptimeInMinutes = (millis() / 1000) / 60;
-
-  // Uptime conversion from second to hour
-  int uptimeInHours = ((millis() / 1000) / 60) / 60;
-
-  // Set the update uptime every 10 seconds
-  if (uptimeInSeconds % 10 == 0)
-  {
-    if (Firebase.setInt(firebaseData, "/FirebaseIOT/uptimeInSeconds", uptimeInSeconds))
-    {
-      Serial.printf("\n+%ds", 10);
-    }
-    else
-    {
-      Serial.println("\nERROR (uptimeInSeconds): " + firebaseData.errorReason());
-    }
-  }
-
-  if (Firebase.setInt(firebaseData, "/FirebaseIOT/uptimeInMinutes", uptimeInMinutes))
-  {
-    Serial.printf("\n+%.2fm", 0.02);
-  }
-  else
-  {
-    Serial.println("\nERROR (uptimeInMinutes): " + firebaseData.errorReason());
-  }
-  
-  if (Firebase.setInt(firebaseData, "/FirebaseIOT/uptimeInHours", uptimeInHours))
-  {
-    Serial.printf("\n+%.4fh\n", 0.0003);
-  }
-  else
-  {
-    Serial.println("\nERROR (uptimeInHours): " + firebaseData.errorReason());
-  }
-}
-
-void setup()
-{
-
+void setup() {
   Serial.begin(9600);
 
-  // Setup a function to be called every second
-  timer.setInterval(1000L, myTimerEvent);
-
   dht.begin();
-  pinMode(led, OUTPUT);
+  pinMode(redLed, OUTPUT);
   
   Blynk.begin(auth, ssid, pass);
 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
-
-  // Set the LED first setup to LOW
-  digitalWrite(led, LOW);
 }
 
-void sensorUpdate()
-{
-  // Reading temperature or humidity takes about 1000 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
+void sensorUpdate() { 
+  h = dht.readHumidity();
+  t = dht.readTemperature();
 
-  // Uptime in seconds
-  int everyTenMinutes = millis() / 1000;
-
-  // Save the data to firebase every 10 minutes 
-  if (everyTenMinutes % 600 == 0)
-  {
-    if (Firebase.pushFloat(firebaseData, "/Logs/humidity", h))
-    {
-      count++;
-      Firebase.setInt(firebaseData, "/Count/times", count);
-    }
-    else
-    {
-      Serial.println("\nERROR (Humidity logs): " + firebaseData.errorReason());
-    }
-  }
-  
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-
-  // Save the data to firebase every 10 minutes 
-  if (everyTenMinutes % 600 == 0)
-  {
-    if (Firebase.pushFloat(firebaseData, "/Logs/temperature", t))
-    {}
-    else
-    {
-      Serial.println("\nERROR (Temperature logs): " + firebaseData.errorReason());
-    }
-  }
+  Serial.printf("Hum  = %d%%", h);
+  Serial.printf("\nTem  = %.1f°C", t);
 
   // Check if the temperature higher than 30°C then turn on the LED
-  if (t >= 30)
-  {
-    digitalWrite(led, LOW);
-    Blynk.virtualWrite(V1, 1);
-    Firebase.setInt(firebaseData, "/FirebaseIOT/led", 1);
-  }
-  else
-  {
-    digitalWrite(led, HIGH);
-    Blynk.virtualWrite(V1, 0);
-    Firebase.setInt(firebaseData, "/FirebaseIOT/led", 0);
+  if (t >= 30) {
+    digitalWrite(redLed, LOW);
+    if (ledStatus == true) {
+      ledStatus = false;
+      Blynk.virtualWrite(V1, 1);
+      Serial.printf("\nBlynk redLed turned on.");
+    }
+  } else {
+    digitalWrite(redLed, HIGH);
+    if (ledStatus == false) {
+      ledStatus = true;
+      Blynk.virtualWrite(V1, 0);
+      Serial.printf("\nBlynk redLed turned off.");
+    }
   }
   
-  // Read temperature as Fahrenheit (isFahrenheit = true)
   float f = dht.readTemperature(false);
-
-  // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t) || isnan(f))
   {
-    Serial.println(F("Failed to read from DHT sensor!"));
+    if (dhtStatus == true) {
+      dhtStatus = false;
+      Blynk.virtualWrite(V0, 0);
+      Serial.printf("\nBlynk dhtStatus failed.");
+    }
     return;
+  } else {
+    if (dhtStatus == false) {
+      dhtStatus = true;
+      Blynk.virtualWrite(V0, 1);
+      Serial.printf("\nBlynk dhtStatus reading.");
+    }
   }
 
-  // Send data of humidity to Blynk  
-  Blynk.virtualWrite(V5, h);
-
-  // Send data of temperature to Blynk
-  Blynk.virtualWrite(V4, t);
-  
-  if (Firebase.setFloat(firebaseData, "/FirebaseIOT/temperature", t))
-  {
-    Serial.printf("\nTemperature : %.1f°C", t);
+  // If the data updated, send to Blynk
+  if (hTemp != h) {
+    Blynk.virtualWrite(V5, h);
+    Serial.printf("\nBlynk Hum updated.");
   }
-  else
-  {
-    Serial.println("\nERROR (Temperature): " + firebaseData.errorReason());
+  if (tTemp != t) {
+    Blynk.virtualWrite(V4, t);
+    Serial.printf("\nBlynk Tem updated.");
   }
-
-  if (Firebase.setFloat(firebaseData, "/FirebaseIOT/humidity", h))
-  {
-    Serial.printf("\nHumidity    : %.1f%%", h);
-  }
-  else
-  {
-    Serial.println("\nERROR (Humidity): " + firebaseData.errorReason());
-  }
+  hTemp = h;
+  tTemp = t;
 }
-void loop()
-{
-  // Run the sensor update function
+
+void saveDataToFirebase() {
+  Firebase.pushFloat(firebaseData, "/Logs/humidity", dht.readHumidity());
+  Firebase.pushFloat(firebaseData, "/Logs/temperature", dht.readTemperature());
+}
+
+void loop() {
   sensorUpdate();
-
-  // Run the Blynk
   Blynk.run();
-  
-  // Run the timer for uptime
-  timer.run();
 
-  // Optional delay
-  // delay(300);
+  theTime = millis() / 1000;
+  if (theTime > beforeTime) {
+    secSS++;
+    if (secSS >= 60) {
+      secSS = 0;
+      minMM++;
+      if (minMM >= 60) {
+        minMM = 0;
+        hrsHH++;
+        if (hrsHH >= 99) {
+          secSS = 0;
+          minMM = 0;
+          hrsHH = 0;
+        }
+      }
+    }
+    beforeTime = theTime;
+  }
+  Serial.printf("\nTime = %02d:%02d:%02d\n\n", hrsHH, minMM, secSS);
+
+  if (theTime % 600 == 0) { saveDataToFirebase(); }
+  delay(1000);
 }
